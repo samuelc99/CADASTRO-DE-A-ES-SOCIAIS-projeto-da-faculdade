246201078d9e0dcfe4c4c15d580535a9023cd893
unknown <samuka1999cristovao@gmail.com>
2025-10-26 18:54:50 -0300

    update1

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://acoessociais.onrender.com",
+baseURL: "https://cadastrodeacoessociaisbackend.onrender.com/api",
 });
